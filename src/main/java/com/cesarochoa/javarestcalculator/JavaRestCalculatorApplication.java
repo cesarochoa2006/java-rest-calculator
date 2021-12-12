04f83d2ba998f5f04fe1de7c9cf48cce4cba8cc3
Cesar <cesarochoa2006@github.com>
2021-12-11 19:32:38 -0500

    [feature] initial files

---
+++ src/main/java/com/cesarochoa/javarestcalculator/JavaRestCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.cesarochoa.javarestcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestCalculatorApplication.class, args);
+	}
+
+}
